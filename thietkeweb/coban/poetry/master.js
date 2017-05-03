ef322490561b30e4538d38281a3546aed4796c9b
SOK KIM THANH <thanhsk1991@gmail.com>
2017-05-03 19:28:39 +0700

    Bước hoàn thành cuối
    Bước hoàn thành cuối thiết kế web bằng HTML, Jquery, CSS

--- thietkeweb/coban/poetry/master.js
+++ thietkeweb/coban/poetry/master.js
@@ -1,3 +1,13 @@
 jQuery(document).ready(function() {
-    alert('vaoday');
+    jQuery("#title").addClass("title");
+    jQuery("#first").addClass("first text-right");
+    jQuery("#next").addClass("next");
+    jQuery("#last").addClass("last text-center");
+    jQuery("#author").addClass("author text-right container");
+    $("ul").addClass("list-unstyled");
+    $("#header").addClass("jumbotron text-center header");
+    $("#body").addClass("container body");
+    $("#footer").addClass("container-fluid footer");
+    var hanmactu_profile = "https://vi.wikipedia.org/wiki/H%C3%A0n_M%E1%BA%B7c_T%E1%BB%AD";
+    $("a").attr('href', hanmactu_profile);
 });
